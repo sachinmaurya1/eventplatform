baaf0eb75463e7b9007f103585093ce3cc2087d5
sachinmaurya1 <ajaymaurya18329@gmail.com>
2024-02-26 13:21:41 +0530

    final commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Evently is a platform for event management.',
+  title: 'EventXpo',
+  description: 'EventXpo is a platform for event management.',
   icons: {
     icon: '/assets/images/logo.svg'
   }
